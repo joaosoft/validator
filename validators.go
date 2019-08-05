f9b6941f039f66605915a9d49d2927e475a85a4c
João Ribeiro <joao.ribeiro@foursource.pt>
2019-08-05 12:31:29 +0100

    fix

--- validators.go
+++ validators.go
@@ -552,6 +552,15 @@ func (v *Validator) validate_error(context *ValidatorContext, validationData *Va
 }
 
 func (v *Validator) validate_id(context *ValidatorContext, validationData *ValidationData) []error {
+	id := v._convertToString(validationData.Expected)
+	dat := &data{
+		value: validationData.Value,
+		typ: reflect.StructField{
+			Type: reflect.TypeOf(validationData.Value),
+		},
+	}
+	context.SetValue(ConstTagId, id, dat)
+
 	return nil
 }
 
