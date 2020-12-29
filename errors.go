7f0d9e6035073ab1953300380aee9c6f97295316
João Ribeiro <joao.ribeiro@foursource.pt>
2020-12-29 19:03:34 +0000

    fix

--- errors.go
+++ errors.go
@@ -3,10 +3,10 @@ package validator
 import "github.com/joaosoft/errors"
 
 var (
-	ErrorSkipValidation     = errors.New(errors.ErrorLevel, 1, "skip validation")
-	ErrorInvalidValue       = errors.New(errors.ErrorLevel, 2, "invalid value")
-	ErrorInvalidPointer     = errors.New(errors.ErrorLevel, 3, "invalid pointer")
-	ErrorInvalidTag         = errors.New(errors.ErrorLevel, 4, "invalid tag [%s]")
-	ErrorInvalidTagArgument = errors.New(errors.ErrorLevel, 5, "invalid tag argument [%s]")
-	ErrorInvalidTagPrefix   = errors.New(errors.ErrorLevel, 6, "invalid prefix [%s] on tag [%s]")
+	ErrorSkipValidation     = errors.New(errors.LevelError, 1, "skip validation")
+	ErrorInvalidValue       = errors.New(errors.LevelError, 2, "invalid value")
+	ErrorInvalidPointer     = errors.New(errors.LevelError, 3, "invalid pointer")
+	ErrorInvalidTag         = errors.New(errors.LevelError, 4, "invalid tag [%s]")
+	ErrorInvalidTagArgument = errors.New(errors.LevelError, 5, "invalid tag argument [%s]")
+	ErrorInvalidTagPrefix   = errors.New(errors.LevelError, 6, "invalid prefix [%s] on tag [%s]")
 )
